815603a12c218dd6ae5385135a5bcc4cb565b2df
David Skaggs <davidtskaggs@gmail.com>
2018-02-13 11:28:46 -0500

    Add shelfchanger component

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ShelfChanger from './ShelfChanger'
 
 class Book extends Component {
   static propTypes = {
@@ -14,16 +15,7 @@ class Book extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`
             }}></div>
-            {/* TODO: separate compoent for changer */}
-            <div className="book-shelf-changer">
-              <select>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
+            <ShelfChanger />
           </div>
           <div className="book-title">{this.props.book.title}</div>
           {/* TODO: Handle multiple authors */}
