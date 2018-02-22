f76484f3ae51f2ec43c4c98fe61b349791765372
David Skaggs <davidtskaggs@gmail.com>
2018-02-22 11:16:51 -0500

    Refactor app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import BookShelf from './BookShelf'
 import ListBooks from './ListBooks';
+import Search from './Search'
 
 import './App.css';
 
@@ -16,8 +17,8 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
     query: '',
-    books: [],
-    showSearchPage: false
+    books: []
+    // showSearchPage: false
 
   }
 
@@ -40,47 +41,15 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+        <Route exact path="/" render={() =>(
+          <BookShelf books={this.state.books}/>
+        )}/>
+        <Route path="/search" render={() =>(
+          <Search />
+        )}/>
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
+          
 
-                <input
-                  type="text"
-                  placeholder="Search by title or author"
-                  value={(this.state.query)}
-                  onChange={(event) => this.updateQuery(event.target.value)}
-                />
-                {/* Remove JSON.stringify line after finished debugging */}
-                {JSON.stringify(this.state.query)}
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <BookShelf books={this.state.books}/>  
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
       </div>
     )
   }
